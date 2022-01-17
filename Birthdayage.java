944bba4c2ae994a4fe4a1ebd41ab4b3d7d4c6105
mohanapriyabtech <mohana.priya@sparkouttech.com>
2022-01-17 13:12:03 +0530

    Basics

--- Birthdayage.java
+++ Birthdayage.java
@@ -7,27 +7,24 @@ public class Birthdayage {
 	public static void main(String[] args, String[] a1) {
 		// TODO Auto-generated method stub
 		
-		
-		
-        // Loop to store input values in nums array
-        
-            Scanner c = new Scanner(System.in);
+	        Scanner c = new Scanner(System.in);
    	    
             int x = 2020;
    	     	int age ;
-   	     	String name ,year;
-   	       // String[] a ;
-   	        
+   	     	
+   	     	
    	     	System.out.println("Enter the name and year");
    	     	String n = c.nextLine();
-   	     	String[] a = n.split(" ");
+   	     	String[] d= n.split("  ");
    	     	
-   	     	name = a[0];
-   	     	year = a[1];
-   	     	
-   	     	int i = Integer.parseInt(year);
+   	     
+   	     	String[] a = new String[d];
+   	       String n2 =  d[0];
+   	       String n3 = d[1];
+   	      
+   	     	int i = Integer.parseInt(d[1]);
             age = x-i;
-   	     	System.out.println("Happy Birthday "  +n  +  "! Your current age is " +age);
+   	     	System.out.println("Happy Birthday "  +d[0] +  "! Your current age is " +age);
    	       
    	       
    	       

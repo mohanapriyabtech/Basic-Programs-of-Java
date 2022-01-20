88a7905a897a8bcd981e334652e417d101c455e8
mohanapriyabtech <mohana.priya@sparkouttech.com>
2022-01-20 18:40:00 +0530

    class

---
+++ Students.java
@@ -0,0 +1,26 @@
+package pack1;
+
+import pack.School; ;
+
+
+ class Students {
+
+	public static void main(String[] args) {
+	
+		 School s= new School();
+		 
+		 s.msg();
+		 
+		 s.Studentdetail("Raja ", 119);
+		// s.Studentdetail("arun ", 120);
+		 
+		 s.addmarks(80,60,50);
+		 
+		 System.out.println(s);
+		 
+		 
+		
+
+	}
+
+}
